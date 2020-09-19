fd18d423c60f9e49ce01977bf3a035efb960cee5
selarassolusindo_on_lenovo_i3 <project.selusin@gmail.com>
2020-09-19 21:43:28 +0700

    .

--- application/modules/package/controllers/Package.php
+++ application/modules/package/controllers/Package.php
@@ -36,20 +36,20 @@ class Package extends CI_Controller
             ->display_as('PN1DN', '1 Night (DN)')
             ;
 
-        $crud->callback_add_field('SN3LN', function() {
-            return 'USD <input type="text" maxlength="50" value="" name="SN3LN">';
+        $crud->callback_field('SN3LN', function($value = '', $primary_key = null) {
+            return 'USD <input type="text" maxlength="50" value="'.$value.'" name="SN3LN">';
         });
-        $crud->callback_add_field('SN6LN', function() {
-            return 'USD <input type="text" maxlength="50" value="" name="SN6LN">';
+        $crud->callback_field('SN6LN', function($value = '', $primary_key = null) {
+            return 'USD <input type="text" maxlength="50" value="'.$value.'" name="SN6LN">';
         });
-        $crud->callback_add_field('SNELN', function() {
-            return 'USD <input type="text" maxlength="50" value="" name="SNELN">';
+        $crud->callback_field('SNELN', function($value = '', $primary_key = null) {
+            return 'USD <input type="text" maxlength="50" value="'.$value.'" name="SNELN">';
         });
-        $crud->callback_add_field('PN1LN', function() {
-            return 'USD <input type="text" maxlength="50" value="" name="PN1LN">';
+        $crud->callback_field('PN1LN', function($value = '', $primary_key = null) {
+            return 'USD <input type="text" maxlength="50" value="'.$value.'" name="PN1LN">';
         });
-        $crud->callback_add_field('PN1DN', function() {
-            return 'IDR <input type="text" maxlength="50" value="" name="PN1DN">';
+        $crud->callback_field('PN1DN', function($value = '', $primary_key = null) {
+            return 'IDR <input type="text" maxlength="50" value="'.$value.'" name="PN1DN">';
         });
 
         $crud
@@ -65,12 +65,29 @@ class Package extends CI_Controller
             'SN3C', 'SN3CP', 'SN6C', 'SN6CP', 'SNEC', 'SNECP', 'PN3C', 'PN3CP', 'PN6C', 'PN6CP',
             'PNEC', 'PNECP'
             );
+        $crud->change_field_type('SN3C', 'invisible');
+        $crud->change_field_type('SN3CP', 'invisible');
+        $crud->change_field_type('SN6C', 'invisible');
+        $crud->change_field_type('SN6CP', 'invisible');
+        $crud->change_field_type('SNEC', 'invisible');
+        $crud->change_field_type('SNECP', 'invisible');
+        $crud->change_field_type('PN3C', 'invisible');
+        $crud->change_field_type('PN3CP', 'invisible');
+        $crud->change_field_type('PN6C', 'invisible');
+        $crud->change_field_type('PN6CP', 'invisible');
+        $crud->change_field_type('PNEC', 'invisible');
+        $crud->change_field_type('PNECP', 'invisible');
 
-        $crud->callback_before_insert();
+        $crud->callback_before_insert(array($this, 'updateCost'));
 
         $output = $crud->render();
         $output->_caption = 'Package';
         $this->_example_output($output);
+    }
+
+    public function updateCost()
+    {
+
     }
 
     public function valueToUsd($value, $row)
