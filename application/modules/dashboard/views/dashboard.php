bc7c2af8882efd61ba288cffb1675c25c49bcce6
selarassolusindo_on_lenovo_i3 <project.selusin@gmail.com>
2020-10-01 18:04:38 +0700

    .

--- application/modules/dashboard/views/dashboard.php
+++ application/modules/dashboard/views/dashboard.php
@@ -6,12 +6,12 @@
       <div class="inner">
         <h3>150</h3>
 
-        <p>New Orders</p>
+        <p>Saldo Awal</p>
       </div>
       <div class="icon">
         <i class="ion ion-bag"></i>
       </div>
-      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+      <a href="<?php echo site_url('saldo-awal'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
     </div>
   </div>
   <!-- ./col -->
